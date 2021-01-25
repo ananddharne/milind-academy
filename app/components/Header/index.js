fe741816a8a528f9a6da32d98ae72225d4d731cd
anand.dharne24@gmail.com <anand@gethappie.me>
2021-01-24 20:25:54 -0500

    add reviews

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -16,7 +16,7 @@ import {
   LoginOutlined,
   LogoutOutlined
 } from "@ant-design/icons";
-import { Menu } from "antd";
+import { Menu, Carousel } from "antd";
 import SocialMediaSection from "./SocialMediaSection";
 import A from "./A";
 import "./index.css";
@@ -213,6 +213,71 @@ function Header() {
         </Menu.Item>
       </Menu>
       <div className="students-image" />
+      <div className="carousel">
+      <Carousel autoplay>
+
+        <div className="carousel-item-div">
+        <h1 style={{color: 'white', fontSize: '180%', color: '#ED7170'}}> OUR GLOWING REVIEWS! </h1>
+        <h1 style={{color: 'white'}}>
+          Deepika
+        </h1>
+        <p>
+        ⭐⭐⭐⭐⭐
+        </p>
+        <p>
+          <i style={{fontSize: '120%'}}>
+          Superb class for engineering students. Milind sir teaches very well and clears all the doubts of students.
+          </i>
+        </p>
+        <h1 style={{color: 'white', fontSize: '110%'}}> <i> As seen on <a style={{color: '#ED7170'}} href="https://www.justdial.com/Pune/Milind-Academy-Opp-Bhavkar-Bhavan-Shivaji-Nagar/020PXX20-XX20-090626131541-H9A8_BZDET" target="_blank"> Justdial </a></i> </h1>
+        </div>
+        <div className="carousel-item-div">
+        <h1 style={{color: 'white', fontSize: '180%', color: '#ED7170'}}> OUR GLOWING REVIEWS! </h1>
+        <h1 style={{color: 'white'}}>
+          Ankit Mohite
+        </h1>
+        <p>
+        ⭐⭐⭐⭐
+        </p>
+        <p>
+          <i style={{fontSize: '120%'}}>
+          Best Class for diploma and engineering technical subjects,teaching method of milind sir is very good,each topic explained in detail and all doubts is cleared after every class. Overall very nice experience here
+          </i>
+        </p>
+        <h1 style={{color: 'white', fontSize: '110%'}}> <i> As seen on <a style={{color: '#ED7170'}} href="https://www.justdial.com/Pune/Milind-Academy-Opp-Bhavkar-Bhavan-Shivaji-Nagar/020PXX20-XX20-090626131541-H9A8_BZDET" target="_blank"> Justdial </a></i> </h1>
+        </div>
+        <div className="carousel-item-div">
+        <h1 style={{color: 'white', fontSize: '180%', color: '#ED7170'}}> OUR GLOWING REVIEWS! </h1>       
+        <h1 style={{color: 'white'}}>
+          Hrishikesh Kulkarni
+        </h1>
+        <p>
+        ⭐⭐⭐⭐⭐
+        </p>
+        <p>
+          <i style={{fontSize: '120%'}}>
+          This is one of the best class for diploma n degree.I personally joined it,for diploma n degree too.If your concepts are not clear or you find difficulties in the understand you must join it.It start with basic.
+          </i>
+        </p>
+        <h1 style={{color: 'white', fontSize: '110%'}}> <i> As seen on <a style={{color: '#ED7170'}} href="https://www.justdial.com/Pune/Milind-Academy-Opp-Bhavkar-Bhavan-Shivaji-Nagar/020PXX20-XX20-090626131541-H9A8_BZDET" target="_blank"> Justdial </a></i> </h1>
+        </div>
+        <div className="carousel-item-div">
+        <h1 style={{color: 'white', fontSize: '180%', color: '#ED7170'}}> OUR GLOWING REVIEWS! </h1>        
+        <h1 style={{color: 'white'}}>
+          Harsh Munot
+        </h1>
+        <p>
+        ⭐⭐⭐⭐
+        </p>
+        <p>
+          <i style={{fontSize: '120%'}}>
+          This is one of the best class for degree and diploma. I joined it for diploma, they always teach you from basic and if there are any doubts, they always clear it. One must join this class if there is need to complete diploma or degree
+          </i>
+        </p>
+        <h1 style={{color: 'white', fontSize: '110%'}}> <i> As seen on <a style={{color: '#ED7170'}} href="https://www.justdial.com/Pune/Milind-Academy-Opp-Bhavkar-Bhavan-Shivaji-Nagar/020PXX20-XX20-090626131541-H9A8_BZDET" target="_blank"> Justdial </a></i> </h1>
+        </div>
+      </Carousel>
+      </div>
     </div>
   );
 }
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,7 +10,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
-
+import { Menu, Carousel } from "antd";
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
@@ -51,6 +51,7 @@ function App() {
 
 
       </Switch>
+
       <Footer />
     </AppWrapper>
   );
