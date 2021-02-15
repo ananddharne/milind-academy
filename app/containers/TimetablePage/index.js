bf7263cbb1f6c53d1fbf85880418525267b42a79
Anand Dharne <adharne@ADharne-S122.local>
2021-02-15 00:25:34 -0500

    amplify apis

---
+++ app/containers/TimetablePage/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import EditableTable from 'components/EditableTable';
+import {Card} from 'antd'
+import "./index.css"
+
+
+
+export default function EnggCoursesPage() {
+
+  
+  return (
+    <EditableTable/>
+  );
+}
